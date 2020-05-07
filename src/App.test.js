d04ef4148ec6e66c0048b51c9a6f1f306fdcf510
wtrawlings <rawlings.warren@gmail.com>
2020-05-07 18:08:38 -0400

    trying to follow the lecture

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+//import the app?
+import { App } from './App';
+
+// test('explain the test', () => {
+
+// })
+
---
+++ src/components/Episodes.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { Episodes } from './Episodes';
+
+// test('explain the test', () => {
+    
+// })
+test('Episodes renders correctly', () => {
+    const mockGetData = jest.fn();
+    const { getByText } = render(
+        <Episodes getData={mockGetData} isFetchingData = {false} />
+
+    );
+    getByText(/chapter one/i)
+    //this is the long version of the test line below.
+    //expect(getByText(/select a season/i)).toBeInTheDocument();
+})
